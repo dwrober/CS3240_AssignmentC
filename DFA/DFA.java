e6e8c4a64fa1a8b5f69a64fe88eab2bbd9333fcc
David <dwrober@gmail.com>
2015-04-04 22:42:40 -0600

    added most of the required documentation, tests still fail

--- DFA/DFA.java
+++ DFA/DFA.java
@@ -1,33 +1,115 @@
+import java.util.Set;
+import java.util.Map;
 
 /**
- * Write a description of class DFA here.
+ * Java implementation of deterministic finite state automata.
+ * <p>
+ * A trap state is implicit; that is, any unspecified transition is treated as ending in a trap state from which there is no exit.
+ * <p>
+ * The transition function is represented as a Map of Maps. The outer map associates a state with its state-specific transition function subset (a transition table row) which is itself represented as a mapping from input symbol to destination state.
+ * <p>
+ * Note that unspecified cases have undefined results.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author David Robertson - built according to http://jodypaul.com/cs/theory/DFAjava/DFA.html>required api
+ * @version 1.0.0
  */
-public class DFA
+public class DFA extends Object
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    /** the set of states of this DFA */
+    private Set<State> states;
 
+    /** the alphabet of this DFA */
+    private Set<String> alphabet;
+
+    /** the transition function of this DFA */
+    private Map<State,Map<String,State>> transition;
+    
+    /** the start state of this DFA */
+    private State start;
+    
+    /** the start state of this DFA */
+    private Set<State> accept;
+    
     /**
-     * Constructor for objects of class DFA
+     * Fully parameterized constructor for DFA objects.
+     * @param states the set of states of this DFA
+     * @param alphabet the alphabet of this DFA
+     * @param transition the transition function of this DFA
+     * @param start the start state of this DFA
+     * @param accept the set of accept states of this DFA
      */
-    public DFA()
+    public DFA(Set<State> states, Set<String> alphabet, Map<State,Map<String,State>> transition, State start, Set<State> accept)
     {
-        // initialise instance variables
-        x = 0;
+        this.states = states;
+        this.alphabet = alphabet;
+        this.transition = transition;
+        this.start = start;
+        this.accept = accept;
+    }
+    
+    /**
+     * Determines whether given string is accepted or rejected by this DFA. Each state's activity couner is initialized to zero, then incremented by one each time the state is entered.
+     * @param input the input string; allows both "" and null to indicate the empty string ε
+     * @return true if the string is in the language recognized by this DFA; false otherwise
+     */
+    public boolean accepts(String input)
+    {
+        return false;
+    }
+    
+    /**
+     * Retrieves the set of accept states.
+     * @return accept states
+     */
+    public Set<State> acceptStates()
+    {
+        return null;
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Retrieves the alphabet of this DFA.
+     * @return the alphabet
      */
-    public int sampleMethod(int y)
+    public Set<String> alphabet()
     {
-        // put your code here
-        return x + y;
+        return null;
+    }
+    
+    /**
+     * Retrieves the initial state of this DFA, if any.
+     * @return the initial state; null if none
+     */
+    public State initialState()
+    {
+        return null;
+    }
+    
+    /**
+     * Lookup transition for specified state and input.
+     * @param source the source state
+     * @param input the input symbol
+     * @return the destination state; null if none
+     */
+    public State nextState(State source, String input)
+    {
+        return null;
+    }
+    
+    /**
+     * Retrieves the set of all states.
+     * @return the states
+     */
+    public Set<State> states()
+    {
+        return null;
+    }
+    
+    /**
+     * Retrieves the transition function of this DFA.
+     * @return Retrieves the transition function of this DFA.
+     */
+    public Map<State,Map<String,State>> transitionFunction()
+    {
+        return null;
     }
 }
